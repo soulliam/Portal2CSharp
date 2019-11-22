1083075485c6e7828017ed62a59feb2e41ca1b37
MGoode <MGoode@thefastpark.com>
2019-11-22 11:27:07 -0500

    Added Crypt class and testing

---
+++ App_Code/clsCrypt.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+/// <summary>
+/// Summary description for clsCrypt
+/// </summary>
+public class clsCrypt
+{
+    public clsCrypt()
+    {
+
+    }
+    public static string Encrypt(string clearText)
+    {
+        byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+        using (Aes encryptor = Aes.Create())
+        {
+            string thisKey = System.Configuration.ConfigurationManager.AppSettings["exPartex"];
+            Random rand = new Random();
+            Byte[] IV = new byte[15];
+            rand.NextBytes(IV);
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(thisKey, IV);
+            encryptor.Key = pdb.GetBytes(32);
+            encryptor.IV = pdb.GetBytes(16);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(clearBytes, 0, clearBytes.Length);
+                    cs.Close();
+                }
+                clearText = Convert.ToBase64String(IV) + Convert.ToBase64String(ms.ToArray());
+            }
+        }
+        return clearText;
+    }
+
+    public static string Decrypt(string cipherText)
+    {
+        string thisKey = System.Configuration.ConfigurationManager.AppSettings["exPartex"];
+        byte[] IV = Convert.FromBase64String(cipherText.Substring(0, 20));
+        cipherText = cipherText.Substring(20).Replace(" ", "+");
+        byte[] cipherBytes = Convert.FromBase64String(cipherText);
+        using (Aes encryptor = Aes.Create())
+        {
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(thisKey, IV);
+            encryptor.Key = pdb.GetBytes(32);
+            encryptor.IV = pdb.GetBytes(16);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(cipherBytes, 0, cipherBytes.Length);
+                    cs.Close();
+                }
+                cipherText = Encoding.Unicode.GetString(ms.ToArray());
+            }
+        }
+        return cipherText;
+    }
+}
---
+++ InsuranceFileUploadDownload.aspx.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Web;
+using System.Net;
+using System.Web.UI.WebControls;
+
+public partial class InsuranceFileUploadDownload : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+        }
+        if ((string)Session["LoginError"] != "" || (string)Session["LoginError"] == "null")
+        {
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "scriptkey", "alert('" + Session["LoginError"] + "');");
+        }
+
+        Session["LoginError"] = "";
+
+    }
+
+
+    private void PopulateTreeView(TreeView tree)
+    {
+        try
+        {
+            ImpersonationHelper.Impersonate("PCA", Session["username"].ToString(), Session["password"].ToString(), delegate
+            {
+                string Dir = @"\\park12\\Insurance\1_New PCA Portal Claims\";
+                DirectoryInfo di = new DirectoryInfo(Dir);
+                TreeNode tds = new TreeNode();
+                tds.Text = "<div style='color: red'>" + di.Name + "</div>";
+                tree.Nodes.Add(tds);
+                tds.Text = di.Name;
+                tds.ToolTip = di.FullName;
+                LoadFiles(Dir, tds);
+                LoadSubDirectories(Dir, tds);
+            });
+            tree.CollapseAll();
+        }
+        catch (Exception ex)
+        {
+            Session["LoginError"] = ex.ToString();
+        }
+    }
+
+    private void LoadSubDirectories(string dir, TreeNode td)
+    {
+        // Get all subdirectories  
+        string[] subdirectoryEntries = Directory.GetDirectories(dir);
+        // Loop through them to see if they have any other subdirectories  
+        foreach (string subdirectory in subdirectoryEntries)
+        {
+
+            DirectoryInfo di = new DirectoryInfo(subdirectory);
+            TreeNode tds = new TreeNode();
+            tds.Text = di.FullName;
+            td.ChildNodes.Add(tds);
+            tds.Text = "<div style='color: red'>" + di.Name + "</div>";
+            tds.ToolTip = di.FullName;
+            LoadFiles(subdirectory, tds);
+            LoadSubDirectories(subdirectory, tds);
+        }
+    }
+
+    private void LoadFiles(string dir, TreeNode td)
+    {
+        string[] Files = Directory.GetFiles(dir, "*.*");
+
+        // Loop through them to see files  
+        foreach (string file in Files)
+        {
+            FileInfo fi = new FileInfo(file);
+            TreeNode tds = new TreeNode();
+            tds.Text = fi.Name;
+            tds.ToolTip = fi.FullName;
+            td.ChildNodes.Add(tds);
+        }
+    }
+
+    protected void UploadButton_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            SaveFile(FileUpload1.PostedFile);
+        }
+        else
+        {
+            UpLoadLabel.Text = "You did not specify a file to upload.";
+        }
+
+
+    }
+
+    protected void DownloadButton_Click(object sender, EventArgs e)
+    {
+        if (DownLoadLabel.Text != "")
+        {
+            TreeView1.CollapseAll();
+            TreeView2.CollapseAll();
+            SaveDownFile();
+        }
+        else
+        {
+            DownLoadLabel.Text = "You did not specify a file to download.";
+        }
+
+    }
+
+    void SaveDownFile()
+    {
+        string fnPath = DownLoadLabel.Text;
+        string[] fnParts = fnPath.Split('\\');
+        string fn = fnParts[fnParts.Length - 1];
+
+        string SaveLocation = Server.MapPath(@"~\workingFolder\" + fn);
+
+        ImpersonationHelper.Impersonate("PCA", Session["username"].ToString(), Session["password"].ToString(), delegate
+        {
+            File.Copy(DownLoadLabel.Text, SaveLocation);
+        });
+
+        if (SaveLocation != "")
+        {
+            System.IO.FileInfo file = new System.IO.FileInfo(SaveLocation);
+            if (file.Exists)
+            {
+                Response.Clear();
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+                Response.AddHeader("Content-Length", file.Length.ToString());
+                Response.ContentType = "application/octet-stream";
+                Response.WriteFile(file.FullName);
+                Response.Flush();
+                HttpContext.Current.ApplicationInstance.CompleteRequest();
+            }
+            else
+            {
+                Response.Write("This file does not exist.");
+            }
+        }
+
+        if (File.Exists(SaveLocation))
+        {
+            System.IO.File.Delete(SaveLocation);
+        }
+
+    }
+
+    void SaveFile(HttpPostedFile file)
+    {
+        string fn = System.IO.Path.GetFileName(FileUpload1.PostedFile.FileName);
+        string SaveLocation = Server.MapPath(@"~\workingFolder\" + fn);
+
+        if ((FileUpload1.PostedFile != null) && (FileUpload1.PostedFile.ContentLength > 0))
+        {
+
+            try
+            {
+                FileUpload1.PostedFile.SaveAs(SaveLocation);
+                TreeView1.Nodes.Clear();
+                TreeView2.Nodes.Clear();
+                PopulateTreeView(TreeView1);
+                PopulateTreeView(TreeView2);
+                Response.Write("The file has been uploaded.");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message);
+            }
+        }
+        else
+        {
+            Response.Write("Please select a file to upload.");
+        }
+
+
+
+        string[] fileName = FileUpload1.FileName.Split('.');
+        var path = Server.MapPath(@"~\workingFolder\" + fileName[0] + '.' + fileName[1]);
+
+        ImpersonationHelper.Impersonate("PCA", Session["username"].ToString(), Session["password"].ToString(), delegate
+        {
+            string[] Directories = Directory.GetFiles(UpLoadLabel.Text);
+
+            if (!File.Exists(UpLoadLabel.Text + "\\" + fileName[0] + '.' + fileName[1]))
+            {
+                File.Copy(path, UpLoadLabel.Text + "\\" + fileName[0] + '.' + fileName[1]);
+            }
+        });
+
+
+        string strFileFullPath = SaveLocation;
+
+        if (System.IO.File.Exists(strFileFullPath))
+        {
+            System.IO.File.Delete(strFileFullPath);
+        }
+
+        TreeView1.Nodes.Clear();
+        TreeView2.Nodes.Clear();
+        PopulateTreeView(TreeView1);
+        PopulateTreeView(TreeView2);
+    }
+
+    protected void refresh_Click(object sender, EventArgs e)
+    {
+        TreeView1.Nodes.Clear();
+        TreeView2.Nodes.Clear();
+        PopulateTreeView(TreeView1);
+        PopulateTreeView(TreeView2);
+    }
+
+    protected void Login_Click(object sender, EventArgs e)
+    {
+        Session["username"] = UserName.Text;
+        Session["password"] = Password.Text;
+        PopulateTreeView(TreeView1);
+        PopulateTreeView(TreeView2);
+    }
+}
